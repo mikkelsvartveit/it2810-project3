30e6563861c9e4e24755a4d29091d57a8b9b4829
stianjsu <stianjsu@gmail.com>
2022-10-14 09:50:51 +0200

    #8: basic apollo client setup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,13 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://rickandmortyapi.com/graphql",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
 

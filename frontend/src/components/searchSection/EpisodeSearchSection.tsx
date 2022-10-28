a20d49e2e1a6986b0c3e4afaf0bdf23fbb118857
Jonah Niklas Bjørgsvik Wiecek <jnwiecek@stud.ntnu.no>
2022-10-28 13:19:43 +0200

    #29 feat: added datepicker to filter episodes

--- frontend/src/components/searchSection/EpisodeSearchSection.tsx
+++ frontend/src/components/searchSection/EpisodeSearchSection.tsx
@@ -1,5 +1,12 @@
 import { useReactiveVar } from "@apollo/client";
-import { FormControl, Grid, MenuItem, InputLabel, Select } from "@mui/material";
+import {
+  FormControl,
+  Grid,
+  MenuItem,
+  InputLabel,
+  Select,
+  TextField,
+} from "@mui/material";
 import { useState } from "react";
 import {
   activeEpisodeFilterNameVar,
@@ -7,12 +14,16 @@ import {
   activeEpisodeSortVar,
 } from "../../gql/cache";
 import { SortSelect, TextFieldWithDebounce } from "../formFields";
+import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 export interface IEpisodeSearchSectionProps {}
 
 export function EpisodeSearchSection(props: IEpisodeSearchSectionProps) {
   const episodeFilter = useReactiveVar(activeEpisodeFilterVar);
   const [seasonFilterValue, setseasonFilterValue] = useState("");
+  const [dates, setDates] = useState([null, null]);
 
   const sortOptions = [
     { value: "default", label: "Default order" },
@@ -33,7 +44,7 @@ export function EpisodeSearchSection(props: IEpisodeSearchSectionProps) {
         />
       </Grid>
 
-      <Grid item xs={12} sm={6}>
+      <Grid item xs={6} sm={3}>
         <FormControl fullWidth>
           <InputLabel id="season-label">Season</InputLabel>
           <Select
@@ -63,7 +74,38 @@ export function EpisodeSearchSection(props: IEpisodeSearchSectionProps) {
         </FormControl>
       </Grid>
 
-      <Grid item xs={12} sm={6}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Grid item xs={6} sm={3}>
+          <DesktopDatePicker
+            label="Start date"
+            inputFormat="DD/MM/YYYY"
+            value={dates[0]}
+            onChange={(newValue) => {
+              setDates([newValue, dates[1]]);
+              const newFilter = { ...episodeFilter };
+              newFilter.start_date = newValue ? newValue : undefined;
+              activeEpisodeFilterVar(newFilter);
+            }}
+            renderInput={(params) => <TextField {...params} fullWidth />}
+          />
+        </Grid>
+        <Grid item xs={6} sm={3}>
+          <DesktopDatePicker
+            label="End date"
+            inputFormat="DD/MM/YYYY"
+            value={dates[1]}
+            onChange={(newValue) => {
+              setDates([dates[0], newValue]);
+              const newFilter = { ...episodeFilter };
+              newFilter.end_date = newValue ? newValue : undefined;
+              activeEpisodeFilterVar(newFilter);
+            }}
+            renderInput={(params) => <TextField {...params} fullWidth />}
+          />
+        </Grid>
+      </LocalizationProvider>
+
+      <Grid item xs={6} sm={3}>
         <SortSelect
           options={sortOptions}
           defaultOption={defaultSort}
